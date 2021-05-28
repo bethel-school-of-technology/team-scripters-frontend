fd493a22917de56d803914480749fcc84097ff2b
Dacia <rdacia07@gmail.com>
2021-05-28 14:30:44 -0700

    signup and signup model added

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Signup } from '../models/signup';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  model: Signup = new Signup();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('Successful signup', this.model);
   }
-
 }
